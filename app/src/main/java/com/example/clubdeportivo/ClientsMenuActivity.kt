f39d5d5277e9092649923b0c4821076e30d8f3cc
Sato <s.lutereau@hotmail.com>
2025-05-10 22:18:10 +0200

    Creacion algunas Pantallas

---
+++ app/src/main/java/com/example/clubdeportivo/ClientsMenuActivity.kt
@@ -0,0 +1,26 @@
+package com.example.clubdeportivo
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+
+class ClientsMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clients_menu)
+
+        val btnSocios = findViewById<Button>(R.id.btnSocios)
+        val btnNoSocios = findViewById<Button>(R.id.btnNoSocios)
+
+        btnSocios.setOnClickListener {
+            val intent = Intent(this, SociosMenuActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnNoSocios.setOnClickListener {
+            val intent = Intent(this, NoSociosMenuActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
