6a2e34ee01ed74abe74493f44dcb1bb8fabe7e96
Sato <s.lutereau@hotmail.com>
2025-05-11 20:25:58 +0200

    pantalla error

---
+++ app/src/main/java/com/example/clubdeportivo/NoConnectionActivity.kt
@@ -0,0 +1,41 @@
+package com.example.clubdeportivo
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ErrorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_no_connection)
+
+        val btnRetry = findViewById<Button>(R.id.btnRetry)
+        val textViewErrorDetails = findViewById<TextView>(R.id.textViewErrorDetails)
+        val imageViewError = findViewById<ImageView>(R.id.imageViewError)
+
+        val isServerError = intent.getBooleanExtra("IS_SERVER_ERROR", false)
+
+        if (isServerError) {
+            textViewErrorDetails.text = "Problemas técnicos. Intenta luego"
+            imageViewError.setImageResource(R.drawable.technical_background)
+        } else {
+            textViewErrorDetails.text = "Revisa tu conexión"
+            imageViewError.setImageResource(R.drawable.connection_background)
+        }
+
+        btnRetry.setOnClickListener {
+            if (hasInternetConnection()) {
+                finish() // Vuelve a la pantalla anterior
+            }
+        }
+    }
+
+    private fun hasInternetConnection(): Boolean {
+        //logica conexion
+        return false;
+    }
+}
